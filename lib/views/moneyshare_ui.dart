31219f495564564d214831865ce2ec41a12ea0db
unknown <s6219410027@sau.ac.th>
2022-02-11 16:16:25 +0700

    code v.2:do moneyshere

--- lib/views/moneyshare_ui.dart
+++ lib/views/moneyshare_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_moneyshared_sau/views/show_moneyshare_ui.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MoneyshareUI extends StatefulWidget {
@@ -9,6 +10,60 @@ class MoneyshareUI extends StatefulWidget {
 }
 
 class _MoneyshareUIState extends State<MoneyshareUI> {
+  bool? check_tip = false;
+
+  TextEditingController money_ctrl = TextEditingController();
+  TextEditingController person_ctrl = TextEditingController();
+  TextEditingController tip_ctrl = TextEditingController();
+  //เมธอด -> โค้ดแสดง Dialog เตือน โดยจะรับข้อความมาแสดงที่ Dialog
+  showWarningDialog(context, msg) {
+    //เรียกใช้งานฟังก์ชัน showDialog
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Container(
+            color: Colors.deepPurple,
+            padding: EdgeInsets.only(
+              top: 10.0,
+              bottom: 10.0,
+            ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'คำเตือน',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          content: Text(
+            msg,
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'ตกลง',
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +93,7 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                   top: 30.0,
                 ),
                 child: TextField(
+                  controller: money_ctrl,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: 'ป้อนจำนวนเงิน (บาท)',
@@ -68,6 +124,7 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                   top: 15.0,
                 ),
                 child: TextField(
+                  controller: person_ctrl,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: 'ป้อนจำนวนคน (คน)',
@@ -98,8 +155,20 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Checkbox(
-                    onChanged: (value) {},
-                    value: false,
+                    onChanged: (checked) {
+                      //เขียนโค้ดเอาค่าที่ user เลือกที่ส่งมาให้ chacked มากำหนดให้กับ check_tip
+                      setState(() {
+                        check_tip = checked;
+                        if (checked == false){
+                          tip_ctrl.text = '' ;
+                        }
+                      });
+                    },
+                    value: check_tip,
+                    activeColor: Color(0xFF5A026A),
+                    side: BorderSide(
+                      color: Color(0xFF5A026A),
+                    ),
                   ),
                   Text(
                     'ทิปให้พนักงานเสริฟ',
@@ -113,6 +182,7 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                   top: 15.0,
                 ),
                 child: TextField(
+                  controller: tip_ctrl,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: 'ป้อนจำนวนเงินทิป (บาท)',
@@ -140,7 +210,48 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                 height: 25.0,
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  //สร้างตัวแปรมาเก็บผมลัพธ์ที่ได้จากการคำนวณ
+                  double? moneyshere = 0;
+
+                  //ตรวจสอบและคำนวณ
+                  if (money_ctrl.text.isEmpty) {
+                    showWarningDialog(context, 'ป้อนเงินด้วย!!!!');
+                    return;
+                  } else if (person_ctrl.text.length == 0) {
+                    showWarningDialog(context, 'ป้อนจำนวนคนด้วย!!!!');
+                    return;
+                  } else if (check_tip == false) {
+                    //คำนวณแบบไม่มี tip
+                    double? money = double.parse(money_ctrl.text);
+                    int? person = int.parse(person_ctrl.text);
+                    moneyshere = money / person;
+                  } else {
+                    //ตรวจสอบว่าแล้วได้ป้อน tip หรือยัง
+                    if (tip_ctrl.text.isEmpty) {
+                      showWarningDialog(context, 'ป้อนทิปด้วย!!!!');
+                    } else {
+                      //คำนวณแบบมี tip
+                      double? money = double.parse(money_ctrl.text);
+                      int? person = int.parse(person_ctrl.text);
+                      double? tip = double.parse(tip_ctrl.text);
+                      moneyshere = (money+tip) / person;
+                    }
+                  }
+
+                  //ส่งข้อมูลที่ป้อน และที่คำนวณได้ไปแสดงอึกหน้า ShowMoneyshareUI
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ShowmoneyshareUI(
+                        money: double.parse(money_ctrl.text),
+                        person: int.parse(person_ctrl.text),
+                        tip: double.parse(tip_ctrl.text.isEmpty ? '0' : tip_ctrl.text),
+                        moneyshare: moneyshere,
+                      ),
+                    ),
+                  );
+                },
                 child: Text(
                   'คำนวณ',
                 ),
@@ -156,7 +267,15 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                 height: 25.0,
               ),
               ElevatedButton.icon(
-                onPressed: () {},
+                onPressed: () {
+                  //กำหนดค่าของ TextField ทั้ง 3 ตัวให้ว่าง และ CheckBox ไม่ติ๊ก
+                  setState(() {
+                    money_ctrl.text = '';
+                    person_ctrl.text = '';
+                    tip_ctrl.text = '';
+                    check_tip = false;
+                  });
+                },
                 label: Text(
                   'ยกเลิก',
                 ),
@@ -170,6 +289,20 @@ class _MoneyshareUIState extends State<MoneyshareUI> {
                   ),
                   primary: Colors.red,
                 ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Text(
+                'Creted by NINNIN SAU',
+                style: TextStyle(
+                  color: Colors.grey[800],
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
               ),
             ],
           ),
--- lib/views/show_moneyshare_ui.dart
+++ lib/views/show_moneyshare_ui.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ShowmoneyshareUI extends StatefulWidget {
-  const ShowmoneyshareUI({ Key? key }) : super(key: key);
+  double? money;
+  int? person;
+  double? tip;
+  double? moneyshare;
+
+  ShowmoneyshareUI({
+    Key? key,
+    this.money,
+    this.person,
+    this.tip,
+    this.moneyshare,
+  }) : super(key: key);
 
   @override
   _ShowmoneyshareUIState createState() => _ShowmoneyshareUIState();
@@ -10,7 +21,128 @@ class ShowmoneyshareUI extends StatefulWidget {
 class _ShowmoneyshareUIState extends State<ShowmoneyshareUI> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFC1A0F1),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF6B11EE),
+        title: Text(
+          'ผลการแชร์เงิน',
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50.0,
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                height: 100.0,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'จำนวนเงินที่จะหาร',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.money.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'บาท',
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'จำนวนคนที่จะหาร',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.person.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'คน',
+                ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'จำนวนเงินที่ทิป',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.tip.toString(),
+                style: TextStyle(
+                  color: Color(0xFF5A026A),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'บาท',
+                ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                'สรุปหารกันคนละ',
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: MediaQuery.of (context).size.width - 80.0,
+                height: 80.0,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+                child:Text(
+                  widget.moneyshare.toString(),
+                  style: TextStyle(
+                    color: Color(0xFF5A026A),
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'บาท',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
